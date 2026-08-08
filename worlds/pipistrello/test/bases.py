ca514ec52859b88c5bc4fd4b4600633071380302
CertifiedPyro <certifiedpyro1@gmail.com>
2026-08-07 23:18:43 -0700

    Use custom assert_access_dependency()

--- worlds/pipistrello/test/bases.py
+++ worlds/pipistrello/test/bases.py
@@ -1,3 +1,7 @@
+import typing
+from warnings import deprecated
+
+from BaseClasses import CollectionState
 from test.bases import WorldTestBase
 
 from ..world import PipWorld
@@ -6,3 +10,43 @@ from ..world import PipWorld
 class PipTestBase(WorldTestBase):
     game = "Pipistrello and the Cursed Yoyo"
     world: PipWorld
+
+    @typing.override
+    @deprecated("Use assert_access_dependency() instead")
+    def assertAccessDependency(
+        self,
+        locations: list[str],
+        possible_items: typing.Iterable[typing.Iterable[str]],
+        only_check_listed: bool = False,
+    ) -> None:
+        pass
+
+    def assert_access_dependency(
+        self,
+        locations: list[str],
+        possible_items: typing.Iterable[typing.Iterable[str]],
+    ) -> None:
+        """Asserts that the provided locations can't be reached without the listed items but can be reached with any
+        one of the provided combinations"""
+        all_items = [item_name for item_names in possible_items for item_name in item_names]
+
+        state = CollectionState(self.multiworld)
+        self.collect_all_but(all_items, state)
+        for location in locations:
+            self.assertFalse(
+                state.can_reach(location, "Location", self.player), f"{location} is reachable without {all_items}"
+            )
+
+        for item_names in possible_items:
+            # Find specific item, then collect.
+            # This avoids collecting all of an item, for example.
+            for item_name in item_names:
+                item = self.get_item_by_name(item_name)
+                state.collect(item)
+            for location in locations:
+                self.assertTrue(
+                    state.can_reach(location, "Location", self.player), f"{location} not reachable with {item_names}"
+                )
+            for item_name in item_names:
+                item = self.get_item_by_name(item_name)
+                state.remove(item)
--- worlds/pipistrello/test/test_faria.py
+++ worlds/pipistrello/test/test_faria.py
@@ -19,8 +19,7 @@ class TestFariaNormalLogic(PipTestBase):
     def test_water_section(self) -> None:
         self.world.origin_region_name = "FSB (+10,+2) (Main)"
         golden_badge = "SP (-1,+4) (Southeast): Golden Badge"
-        with self.subTest("Test water section is passable with vanilla logic"):
-            self.assertAccessDependency([golden_badge], [["Walk-the-Dog"]], only_check_listed=True)
+        self.assert_access_dependency([golden_badge], [["Walk-the-Dog"]])
 
 
 class TestFariaHardLogic(PipTestBase):
@@ -31,22 +30,18 @@ class TestFariaHardLogic(PipTestBase):
     def test_water_section(self) -> None:
         self.world.origin_region_name = "FSB (+10,+2) (Main)"
         golden_badge = "SP (-1,+4) (Southeast): Golden Badge"
-        ride = self.get_item_by_name("Wall-Ride")
-        dash = self.get_item_by_name("Wall-Dash")
-        ss = self.get_item_by_name("Progressive Skipping Stone Badge")
-        bp = self.get_item_by_name("BP Shard")
+        dog = self.get_item_by_name("Walk-the-Dog").name
+        ride = self.get_item_by_name("Wall-Ride").name
+        dash = self.get_item_by_name("Wall-Dash").name
+        ss = self.get_item_by_name("Progressive Skipping Stone Badge").name
+        bp = self.get_item_by_name("BP Shard").name
 
-        move_configs = {
-            "Wall-Ride + Wall-Dash + SS (not enough BP)": ([ride, dash, ss, bp, bp], False),
-            "Wall-Ride + Wall-Dash + SS": ([ride, dash, ss, bp, bp, bp, bp], True),
-            "Wall-Ride + Wall-Dash + SS+": ([ride, dash, ss, ss, bp, bp], True),
+        possible_items = {
+            "Walk-the-Dog": [dog],
+            "Wall-Ride + Wall-Dash + SS": [ride, dash, ss, bp, bp, bp, bp],
+            "Wall-Ride + SS+": [ride, ss, ss, bp, bp],
         }
-        for name, config in move_configs.items():
-            with self.subTest(f"Test water section with alternative moves: {name}"):
-                self.collect(config[0])
-                self.assertEqual(
-                    self.world.get_location(golden_badge).can_reach(self.multiworld.state),
-                    config[1],
-                    f"Expected move config {name} to return {config[1]}",
-                )
-                self.remove(config[0])
+        self.assert_access_dependency(
+            [golden_badge],
+            possible_items.values(),
+        )
